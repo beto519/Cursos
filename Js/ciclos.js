8b34398dcaa50bf828459898feef3b2a3405c0c6
Jesus Alberto Rivera Lopez <jesusalbertoriveral205530@gmail.com>
2022-06-20 12:25:45 -0500

    Update ciclos.js

--- Js/ciclos.js
+++ Js/ciclos.js
@@ -1,8 +1,33 @@
 
-for (let index = 0; index <= 100 ; index++) {
-    if (index%2 ===0) {
-        console.log("Este numero es par "+index);
+// for (let index = 0; index <= 100 ; index++) {
+//     if (index%2 ===0) {
+//         console.log("Este numero es par "+index);
         
-    } 
+//     } else{
+//         console.log("Este numero es impar "+index);
+//     }
+    
+// }
+
+
+// const carrito = [
+//     {nombre:'Monitor msi 27" pulgadas', precio: 500},
+//     {nombre:'Gabinete asus tuf gt301', precio: 2700},
+//     {nombre:'Mouse logitech g203', precio: 400},
+//     {nombre:'Teclado G213', precio: 900},
+//     {nombre:'Procesador AMD Ryzen 7 5700G', precio: 7000}
+
+// ];
+// for (let index = 0; index < carrito.length; index++) {
+//    console.log(carrito[index].nombre);
+    
+// }
+
+
+/**
+ * While do
+ */
+
+while (condition) {
     
 }
