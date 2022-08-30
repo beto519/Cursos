872db2d781de8f9dcffc9d8d90eefee85880fb33
Jesus Alberto Rivera Lopez <jesusalbertoriveral205530@gmail.com>
2022-08-30 12:03:10 -0600

    Configuraciones para aprender C#

---
+++ NetBases/index.cs
@@ -0,0 +1,42 @@
+/**
+* Curso de c# .net
+*Conceptos
+**/
+
+/**.NET
+*Es una plataforma de desarrollo que sirve para desarrollar diferentes tipos de aplicaciones
+*ya sea de escritorio, juegos, movil o web.
+*No es un lenguaje de programacion, es una plataforma para desarrollo.
+**/
+
+/**
+*Historia
+*.Net llego hasta la version 4.8 o mejor conocida como .Net framework
+*.Net core
+* 
+**/
+
+/**
+*C#
+*Es un lenguaje de programacion multiparadigmas, similar a java.
+**/
+
+/**
+*ASP.NET es un framework para desarrollar aplicaciones web
+*ASP: Active Server Pages.
+**/
+
+/**
+*Visual Studio
+*Es un IDE
+**/
+
+/**
+*Visual Studio code 
+*Es un editor de texto
+**/
+
+/**
+*Para correr una aplicacion desde terminal es necesario usar
+* dotnet run
+**/
--- PrimeraPagina/js/script.js
+++ PrimeraPagina/js/script.js
@@ -1,1 +1,17 @@
-console.log();
+//Formas de seleccionar contenido
+
+//Queryselector
+const heading = document.querySelector('#heading');
+heading.textContent = 'Nuevo contenido';
+console.log(heading);
+
+//querySelectorAll
+const enlaces = document.querySelectorAll('.navegacion a');
+console.log(enlaces);
+enlaces[0].textContent = "cambiar";
+enlaces[0].href = "http://google.com";
+
+
+
+//getElementById
+const heading2 = document.getElementById("heading");
